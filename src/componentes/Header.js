33b6920ad5e2468470ec333a764075bd59d55f09
Eduardo <egarzaserrano@gmail.com>
2022-03-01 08:38:34 -0600

    Hola

---
+++ src/componentes/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () =>{
+  return (
+    <header>
+    <img src="" alt="" />
+    <span id="CirlceOne"><span id="CirlceTwo"></span></span>
+    <nav>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+    </nav>
+    <h1 id="Nombre">Eduardo <br />Garza Serrano</h1>
+    <h5 id="Profecion">Diseñador</h5>
+    <h3>Acerca de Mí</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium atque, magnam quod odit nesciunt esse optio consectetur pariatur perferendis fugit, possimus ratione vel suscipit delectus consequuntur? Commodi id deleniti debitis?</p>
+    </header>
+  )
+  
+} 
+export default Header
