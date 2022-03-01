ca45190595fe3052f18d6905d40487a8a69811b3
Eduardo <egarzaserrano@gmail.com>
2022-03-01 11:23:10 -0600

    SVG

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -3,17 +3,19 @@ import React from 'react'
 const Header = () =>{
   return (
     <header>
-    <img src="" alt="" />
-    <span id="CirlceOne"><span id="CirlceTwo"></span></span>
-    <nav>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
+      <div id="BColor"></div>
+    <img id="logo" src="./componentes/SVG/Edu.svg" alt="" />
+    <nav id="NavCv">
+        <a href="">Instagram</a>
+        <a href="https://www.linkedin.com/in/eduardo-garza-serrano-672881152/">Linkedin</a>
+        <a href="">Portafolio</a>
     </nav>
+    <span id="CirlceOne"><span id="CirlceTwo"></span></span>
     <h1 id="Nombre">Eduardo <br />Garza Serrano</h1>
     <h5 id="Profecion">Diseñador</h5>
-    <h3>Acerca de Mí</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium atque, magnam quod odit nesciunt esse optio consectetur pariatur perferendis fugit, possimus ratione vel suscipit delectus consequuntur? Commodi id deleniti debitis?</p>
+    <h3 id="Acerca">Acerca de Mí</h3>
+    <p  id="AcercaTex"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium atque, magnam quod odit nesciunt esse optio consectetur pariatur perferendis fugit, possimus ratione vel suscipit delectus consequuntur? Commodi id deleniti debitis?</p>
+    
     </header>
   )
   
